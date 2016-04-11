482fd0f367747848b616fc08134a42887599859b
Riguruchan <aaaaaaaaaadddddddddd30@gmail.com>
2016-04-11 16:40:44 +0900

    InitializeUpdate

--- RideTheFlow/RideTheFlow/src/actor/ActorPtr.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <memory>
-
-class Actor;
-typedef std::shared_ptr<Actor>ActorPtr;
--- RideTheFlow/RideTheFlow/src/scene/IScenePtr.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include<memory>
-
-class IScene;
-class TitleScene;
-//シーン用のシェアドポインタ
-using IScenePtr = std::shared_ptr<IScene>;
