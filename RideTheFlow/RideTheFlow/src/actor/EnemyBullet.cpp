0aedf8b9167122afdd41ed901a2dbf2f074b2dd5
shitensyo <taroramen310@gmail.com>
2016-05-22 16:55:44 +0900

    弓が進行方向を向く

--- RideTheFlow/RideTheFlow/src/actor/EnemyBullet.cpp
+++ RideTheFlow/RideTheFlow/src/actor/EnemyBullet.cpp
@@ -15,7 +15,7 @@ time(0),
 speed(3.0f),
 distance(0, 0, 0),
 mPosition(position),
-mScale(3.0f),
+mScale(1.0f),
 coppyPosition(position),
 mToPoint(toPoint),
 rotate(Vector3::Zero)
@@ -69,23 +69,24 @@ void EnemyBullet::Update()
 
 
 	//進行方向を分ける
-	Vector3 vXY = Vector3(vec.x, vec.y, 0.0f).Normalized();
 	Vector3 vXZ = Vector3(vec.x, 0.0f, vec.z).Normalized();
 	Vector3 vYZ = Vector3(0.0f, vec.y, vec.z).Normalized();
 	//回転計算
-	//rotate.x = Math::Degree(Math::Atan2(vYZ.z, vYZ.y));
+	rotate.x = Math::Degree(Math::Atan2(Math::Abs(vYZ.y), Math::Abs(vYZ.z)));
 	rotate.y = Math::Degree(Math::Atan2(vXZ.x, vXZ.z));
-	//rotate.z = Math::Degree(Math::Atan2(vXY.y, vXY.x));
 
 	//マトリックス計算
 	parameter.mat =
 		Matrix4::Scale(mScale) *
+		Matrix4::RotateZ(0) *
+		Matrix4::RotateX(rotate.x) *
+		Matrix4::RotateY(rotate.y) *
 		Matrix4::Translate(mPosition + Vector3(0.0f, coppyPosition.y, 0.0f));
 }
 
 void EnemyBullet::Draw() const
 {
-	Model::GetInstance().Draw(MODEL_ID::ARROW_MODEL, parameter.mat.GetPosition(), 1.0f, rotate, mScale, true);
+	Model::GetInstance().Draw(MODEL_ID::ARROW_MODEL, parameter.mat.GetPosition(), 1.0f, parameter.mat.GetRotateDegree(), mScale, true);
 }
 
 void EnemyBullet::OnCollide(Actor& other, CollisionParameter colpara)
